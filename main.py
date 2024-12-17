ebf5b70a4563214c2a272dd5bc6731e4daf9820f
Raghav <raghavthakar12@gmail.com>
2024-12-17 15:37:36 -0800

    add seed as command line argument

--- main.py
+++ main.py
@@ -9,12 +9,8 @@ import NSGAII
 import KParentNSGAII
 import DMO
 
-random.seed(2024)
-torch.manual_seed(2024)
-numpy.random.seed(2024)
-
 if __name__ == '__main__':
-    assert len(sys.argv) == 5, "Correct usage: python alg_name data_dirpath alg_config env_config"
+    assert len(sys.argv) == 6, "Correct usage: python alg_name data_dirpath alg_config env_config seed"
    
     # Process the command line args
     alg_name = sys.argv[1]
@@ -23,6 +19,7 @@ if __name__ == '__main__':
     data_dir = data_dir+'/' if data_dir[-1]!='/' else data_dir # Add a directory '/' at the end
     src_alg_config_filename = sys.argv[3]
     src_env_config_filename = sys.argv[4]
+    seed_val = int(sys.argv[5])
 
     # Datetime for file naming
     datetime_now_string = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -34,6 +31,11 @@ if __name__ == '__main__':
     shutil.copyfile(src_alg_config_filename, dest_alg_config_filename)
     dest_env_config_filename = data_dir+alg_name+'_'+datetime_now_string+'_envconfig.yaml'
     shutil.copyfile(src_env_config_filename, dest_env_config_filename)
+
+    # Set the seed value for all libraries
+    random.seed(seed_val)
+    torch.manual_seed(seed_val)
+    numpy.random.seed(seed_val)
 
     # Initialise the algorithm based on alg name
     if alg_name == 'nsga2':
