d688897c0e1c758a01b76f2b3c98cf29a5aad643
Raghav <raghavthakar12@gmail.com>
2024-12-17 15:39:09 -0800

    Explicit uniqueness preservation in all algs

--- NSGAII.py
+++ NSGAII.py
@@ -32,16 +32,20 @@ class NSGAII(Algorithm.Algorithm):
         
         # Sort the population according to fitness
         sorted_indices = pg.sort_population_mo(points=[ind.fitness for ind in self.pop])
-        # print(sorted_indices)
-        for i in sorted_indices:
-            print(self.pop[i])
-        print("----------------------------")
-        # Save only the top half of the population
-        sorted_indices = sorted_indices[:len(sorted_indices)//2]
-
-        # Save these elites into the parent set
-        parent_set = [self.pop[elite_idx] for elite_idx in sorted_indices]
-
+        fitness_tuples = [tuple(ind.fitness) for ind in self.pop]
+        
+        # Track unique and duplicate fitnesses
+        seen = set()
+        unique_indices = []
+        duplicates = []
+        # Separate population into unique and duplicate fitnesses
+        for idx in sorted_indices:
+            ft = fitness_tuples[idx]
+            (unique_indices if ft not in seen else duplicates).append(idx)
+            seen.add(ft)
+        # Pick the top half, but prioritise unique solutions then duplicates
+        parent_indices = (unique_indices + duplicates)[: self.pop_size // 2]
+        parent_set = [self.pop[i] for i in parent_indices]
         # Create empty offpring set
         offspring_set = []
 
