6b0fbae96b3c93c30986dd4ffcd2e0225a50abfa
Raghav <raghavthakar12@gmail.com>
2024-12-18 21:07:49 -0800

    Base class for coevoluoitnary algorithms

--- Algorithm.py
+++ Algorithm.py
@@ -5,6 +5,7 @@ import torch
 import numpy
 
 import MORoverInterface
+import Policy
 import Individual
 import Utils
 
@@ -49,6 +50,49 @@ class CentralisedAlgorithm:
         with open(self.config_filename, 'r') as config_file:
             self.config_data = yaml.safe_load(config_file)
             print('[NSGA-II]: YAML config read.')
+
+        self._load_config()
+    
+    def _load_config(self):
+        """Load internal NSGA-II configuration."""
+        self.pop_size = self.config_data['Evolutionary']['pop_size']
+        self.num_gens = self.config_data['Evolutionary']['num_gens']
+
+class CoevolutionaryAlgorithm:
+    def __init__(self, alg_config_filename, rover_config_filename, data_filename):
+        self.config_filename = alg_config_filename
+        self._read_config()
+
+        self.data_filename = data_filename
+        self.data_logger = ExpUtils.DataLogger.DataLogger(data_fields=['gen',
+                                                                       'id',
+                                                                       'fitness',
+                                                                       'trajectory'],
+                                                                       target_filename=self.data_filename)
+
+        self.interface = MORoverInterface.MORoverInterface(rover_config_filename)
+        self.team_size = self.interface.get_team_size()
+        self.num_objs = self.interface.get_num_objs()
+
+        self.pop = [] # NOTE: A list of subpopulations
+        self.glob_eval_counter = 0
+        
+        for _ in range(self.team_size):
+            # Create a subpopulation of policies
+            subpop = [Policy.Policy(self.config_filename,
+                                    self.interface.get_state_size(),
+                                    self.interface.get_action_size()) for _ in range(self.pop_size)]
+            # Add subpop to the population
+            self.pop.append(subpop)
+        
+        # Evo utils
+        self.utils = Utils.Utils(num_objs=self.num_objs)
+        
+    def _read_config(self):
+        """Read and load NSGA-II configuration from the YAML file."""
+        with open(self.config_filename, 'r') as config_file:
+            self.config_data = yaml.safe_load(config_file)
+            print('[NSGA-II+D]: YAML config read.')
 
         self._load_config()
     
