8412fb2b2dc2018dc0afccf4f66af4cf6454c619
Raghav <raghavthakar12@gmail.com>
2024-12-20 16:46:28 -0800

    Functional Beach Domain (KPNSGA2 finds pareto optimal in ~100 gens!!!)

--- MOBeachEnv.py
+++ MOBeachEnv.py
@@ -127,7 +127,7 @@ class MOBeachEnv:
 
 if __name__ == "__main__":
     beach = MOBeachEnv('/home/raghav/Research/GECCO25/DMO/config/MOBeachEnvConfig.yaml')
-    pos = [0, 0, 0, 1, 1, 1]
-    types = [0, 0, 0, 1, 1, 1]
+    pos = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+    types = [0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1]
 
     print(beach.get_global_rewards(pos, types))
